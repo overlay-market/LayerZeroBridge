f1291a3a192aef46495d65766193d7daa8edc349
c-note <dongustavo20@gmail.com>
2022-11-24 17:21:06 +0100

    updating files

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,32 +1,39 @@
 require("@nomicfoundation/hardhat-toolbox");
+require('dotenv').config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.17",
   networks: {
+    mainnet: {
+      url: `https://mainnet.infura.io/v3/429eb57532b54560b1d4cc4201724bf0`,
+      accounts: [
+        process.env.MAINNET_PRIVATE_KEY,
+      ],
+    },
     goerli: {
       url: `https://goerli.infura.io/v3/429eb57532b54560b1d4cc4201724bf0`,
       accounts: [
-        "0x4d35d875115d8255e79ebfb37d65fe080f7c1f311668524c88f75ade0a747b32",
+        process.env.TESTNET_PRIVATE_KEY,
       ],
     },
     arbitrum_testnet: {
       url: "https://goerli-rollup.arbitrum.io/rpc",
       network_id: 421613,
       accounts: [
-        "0x4d35d875115d8255e79ebfb37d65fe080f7c1f311668524c88f75ade0a747b32",
+        process.env.TESTNET_PRIVATE_KEY,
       ],
     },
-    avalanche_testnet: {
-      url: "https://api.avax-test.network/ext/bc/C/rpc",
-      network_id: 43113,
+    arbitrum_mainnet: {
+      url: "https://arb1.arbitrum.io/rpc",
+      network_id: 42161,
       accounts: [
-        "0x4d35d875115d8255e79ebfb37d65fe080f7c1f311668524c88f75ade0a747b32",
+        process.env.MAINNET_PRIVATE_KEY,
       ],
     },
   },
   etherscan: {
-    apiKey: "U772ATWP1Z8J3MNMFUXXFPYD213PEVEXAZ",
+    apiKey: process.env.ETHERSCAN_API_KEY,
   },
 };
-//7YNIM4192HFF7NXSK1SWWBIN3ZI2EYAQCK U772ATWP1Z8J3MNMFUXXFPYD213PEVEXAZ
+
--- test/lz.js
+++ test/lz.js
@@ -1,16 +1,17 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
 const { advanceBlock } = require("./advancement");
-let owner;
-let lzAppA;
-let LzAppB;
-let chainIdSrc;
-let chainIdDst;
-let tokenA;
-let tokenB;
-let layerZeroEndpointMockSrc;
-let layerZeroEndpointMockDst;
 
+let owner,
+ lzAppA,
+ LzAppB,
+ chainIdSrc,
+ chainIdDst,
+ tokenA,
+ tokenB,
+ layerZeroEndpointMockSrc,
+ layerZeroEndpointMockDst;
+ 
 describe("bridgeToken", function () {
   beforeEach(async function () {
     const accounts = await ethers.getSigners();
@@ -27,7 +28,6 @@ describe("bridgeToken", function () {
 
     const TokenMock = await ethers.getContractFactory("Token");
     tokenA = await TokenMock.deploy("Demo1", "D1", "100000000000000000000");
-
     tokenB = await TokenMock.deploy("Demo1", "D1", 0);
 
     // create two LzApp instances
@@ -60,6 +60,7 @@ describe("bridgeToken", function () {
         [LzAppB.address, lzAppA.address]
       )
     ); // for A, set B
+
     await LzAppB.setTrustedRemote(
       chainIdSrc,
       ethers.utils.solidityPack(
@@ -69,7 +70,7 @@ describe("bridgeToken", function () {
     ); // for B, set A
   });
 
-  it("increment the counter of the destination PingPong when paused should revert", async function () {
+  it("should revert if contract is paused", async function () {
     await lzAppA.enable(true);
     await expect(lzAppA.bridgeToken(chainIdDst, 10000)).to.revertedWith(
       "Pausable: paused"
@@ -108,4 +109,77 @@ describe("bridgeToken", function () {
     expect(tokenBBalanceBeforeTx).to.equal("0");
     expect(tokenBBalanceAfterTx).to.equal("0");
   });
+
+  it("should retry payload", async function () {
+    await lzAppA.setMinDstGasLookup(chainIdDst, 35000);
+    const tokenBBalanceBeforeTx = await tokenB.balanceOf(owner.address);
+    
+    let tx = await lzAppA.bridgeToken(chainIdDst, "10000000000000000000", {
+      value: ethers.utils.parseEther("0.5"),
+    });
+
+    let receipt = await tx.wait();
+    let txPayload = await receipt.events[2].args[5];
+    let hashedPayload = ethers.utils.keccak256(txPayload);
+    
+    const path = await layerZeroEndpointMockDst.path();
+    const storedPayload = await layerZeroEndpointMockDst.storedPayload(
+      chainIdSrc,
+      path
+    );
+    
+    await layerZeroEndpointMockDst.retryPayload(chainIdSrc, path, txPayload)
+    const storedPayload1 = await layerZeroEndpointMockDst.storedPayload(
+      chainIdSrc,
+      path
+    );
+
+    const tokenBBalanceAfterTx = await tokenB.balanceOf(owner.address);
+    expect(storedPayload1[2]).to.equal(ethers.constants.HashZero);
+
+    expect(tokenBBalanceBeforeTx).to.equal("0");
+    expect(tokenBBalanceAfterTx).to.equal("10000000000000000000");
+  });
+
+  it("should bridge a token", async function () {
+    await lzAppA.setMinDstGasLookup(chainIdDst, 350000);
+
+    const tokenABalanceBeforeTx = await tokenA.balanceOf(owner.address);
+    const tokenBBalanceBeforeTx = await tokenB.balanceOf(owner.address); 
+    
+    let tx = await lzAppA.bridgeToken(chainIdDst, "10000000000000000000", {
+      value: ethers.utils.parseEther("0.5"),
+    });
+
+    const tokenABalanceAfterTx = await tokenA.balanceOf(owner.address);
+    const tokenBBalanceAfterTx = await tokenB.balanceOf(owner.address);
+
+    expect(tokenABalanceBeforeTx).to.equal("100000000000000000000");
+    expect(tokenABalanceAfterTx).to.equal("90000000000000000000");
+    
+    expect(tokenBBalanceBeforeTx).to.equal("0");
+    expect(tokenBBalanceAfterTx).to.equal("10000000000000000000");
+  })
+
+  it("bridge token to and fro", async function () {
+    await lzAppA.setMinDstGasLookup(chainIdDst, 350000);
+    await LzAppB.setMinDstGasLookup(chainIdSrc, 350000);
+
+    const tokenABalanceBeforeTx = await tokenA.balanceOf(owner.address);
+    const tokenBBalanceBeforeTx = await tokenB.balanceOf(owner.address); 
+    
+    await lzAppA.bridgeToken(chainIdDst, "10000000000000000000", {
+      value: ethers.utils.parseEther("0.5"),
+    });
+
+    await LzAppB.bridgeToken(chainIdSrc, "10000000000000000000", {
+      value: ethers.utils.parseEther("0.5"),
+    });
+
+    const tokenABalanceAfterTx = await tokenA.balanceOf(owner.address);
+    const tokenBBalanceAfterTx = await tokenB.balanceOf(owner.address);
+
+    expect(tokenABalanceBeforeTx).to.equal(tokenABalanceAfterTx);
+    expect(tokenBBalanceBeforeTx).to.equal(tokenBBalanceAfterTx);
+  })
 });
